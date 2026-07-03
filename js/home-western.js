07057182ec7e1dfeb7d88a1028f142209e6a6966
Kevin Fowler <krfowler@asu.edu>
2026-07-03 08:49:17 -0700

    Add home-western.js for homepage light theme effects

---
+++ js/home-western.js
@@ -0,0 +1,7 @@
+/*
+ * =====================================================================
+ * HOME WESTERN JS
+ * Homepage-only light theme behavior:
+ * vultures, sheriff/bandit standoff, dust, western back-to-top effects.
+ * =====================================================================
+ */
