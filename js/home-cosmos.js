f47736f01dd83b444d2c3fac6d503cff9cde55f1
Kevin Fowler <krfowler@asu.edu>
2026-07-03 08:48:56 -0700

    Create home-cosmos.js with initial comments
    Add initial comments for home-cosmos.js file.

---
+++ js/home-cosmos.js
@@ -0,0 +1,7 @@
+/*
+ * =====================================================================
+ * HOME COSMOS JS
+ * Homepage-only dark theme behavior:
+ * intro overlay, stars, UFO scroll, beam effects, alien/cosmos interactions.
+ * =====================================================================
+ */
